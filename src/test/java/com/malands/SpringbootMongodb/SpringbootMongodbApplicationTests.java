98da61e47e8a139d90d4f39e82d35202edc36f9a
malansl <malansl@gmail.com>
2019-12-19 15:49:08 -0300

    primeiro commit

---
+++ src/test/java/com/malands/SpringbootMongodb/SpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malands.SpringbootMongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
