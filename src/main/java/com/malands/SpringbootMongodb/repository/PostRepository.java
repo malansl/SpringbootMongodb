938ba46e9c105607dc5dadb739b6587563e39124
malansl <malansl@gmail.com>
2019-12-23 11:54:30 -0300

    Consulta simples com query methods

--- src/main/java/com/malands/SpringbootMongodb/repository/PostRepository.java
+++ src/main/java/com/malands/SpringbootMongodb/repository/PostRepository.java
@@ -1,9 +1,14 @@
 package com.malands.SpringbootMongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.malands.SpringbootMongodb.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String> {}
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+}
